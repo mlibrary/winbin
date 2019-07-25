2f359d0ce531a375e0d8acaab256afc534991770
Ryan Rotter <rrotter@umich.edu>
2019-07-24 23:03:23 -0700

    Print more object detail in userer and grouper

--- grouper.ps1
+++ grouper.ps1
@@ -10,15 +10,19 @@ if (!$group) {
   exit
 }
 
-$found = $true
-try {
-  $null = Get-ADGroup $group
-}
-catch {
-  $found = $false
+function announce($label, $data){
+  if($data){ echo "${label}: $data" }
 }
 
-if ($found) {
+$group_obj = $false
+try {
+  $group_obj = Get-ADGroup $group -Properties *
+}
+catch {
+  # no luck
+}
+
+if ($group_obj) {
   # add or remove member if we have a command to do so
   foreach ($member in $members) {
     $user = (Get-ADUser $member)
@@ -34,8 +38,22 @@ if ($found) {
   }
 
   # list final membership
-  Get-ADGroupMember $group | Sort-Object SamAccountName | Select-Object SamAccountName
-} else {
+  $members = (Get-ADGroupMember $group | Sort-Object SamAccountName | Select-Object SamAccountName).SamAccountName
+  announce "accountname    " $group_obj.SamAccountName
+  announce "name           " $group_obj.Name
+  announce "description    " $group_obj.Description
+  announce "displayname    " $group_obj.DisplayName
+  announce "cannonicalname " $group_obj.CanonicalName
+  Write-Output "Members of ${group}:" 
+  foreach($member in $members){
+    Write-Output "  $member"
+  }
+}
+else {
   # no exact match, print what groups we could find
-  Get-ADGroup -Filter "SamAccountName -like '$group*'" | Select-Object SamAccountName
+  $groups = (Get-ADGroup -Filter "SamAccountName -like '$group*'" | Select-Object SamAccountName).SamAccountName
+  Write-Output "No match for '$group', listing prefix matches:"
+  foreach($matching_group in $groups){
+    Write-Output "  $matching_group"
+  }
 }
--- userer.ps1
+++ userer.ps1
@@ -1,6 +1,6 @@
 $user, $verb, $groups = $args
 
-if (!$user) {
+function usage {
   Write-Output 'usage: userer USER [add|del GROUP ...]'
   Write-Output ''
   Write-Output 'userer USER: search for users named USER, print group membership if exact match found'
@@ -10,9 +10,17 @@ if (!$user) {
   exit
 }
 
+function announce($label, $data){
+  if($data){ echo "${label}: $data" }
+}
+
+if (!$user) {
+  usage
+}
+
 $user_obj = $false
 try {
-  $user_obj = Get-ADUser $user
+  $user_obj = Get-AdUser -Identity $user -Properties *
 }
 catch {
   # no luck
@@ -21,6 +29,9 @@ catch {
 if ($user_obj) {
   if ($verb) {
     # add or remove member if we have a command to do so
+    if ( ! $groups ) {
+      usage
+    }
     foreach ($group in $groups) {
       switch -regex ( $verb ) {
         '^add$' {
@@ -29,14 +40,37 @@ if ($user_obj) {
         '^(del(ete)?|rem(ove)?)$' {
           Remove-ADGroupMember $group $user_obj -Confirm:$false
         }
+        default {
+          usage
+        }
       }
     }
   } else {
-    # only print membership when we aren't making changes, as Get-ADPrincipalGroupMembership
+    # only print info about user when we aren't making changes, as Get-ADPrincipalGroupMembership
     # output won't be consistant until several seconds after running Add-ADGroupMember
-    Get-ADPrincipalGroupMembership $user | Sort-Object SamAccountName | Select-Object SamAccountName
+    $groups = Get-ADPrincipalGroupMembership $user | Sort-Object SamAccountName | Select-Object SamAccountName
+    $groups = $groups.SamAccountName
+
+    announce "accountname    " $user_obj.SamAccountName
+    announce "name           " $user_obj.Name
+    announce "description    " $user_obj.Description
+    announce "displayname    " $user_obj.DisplayName
+    announce "cannonicalname " $user_obj.CanonicalName
+    Write-Output "Groups: "
+    foreach($group in $groups){
+      Write-Output "  $group"
+    }
   }
 } else {
   # no exact match, print what groups we could find
-  Get-ADUser -Filter "SamAccountName -like '$user*'" | Select-Object SamAccountName
+  $users = (Get-ADUser -Filter "SamAccountName -like '$user*'" | Select-Object SamAccountName).SamAccountName
+  if($users){
+    Write-Output "No match for '$user', listing prefix matches:"
+    foreach($uname in (Get-ADUser -Filter "SamAccountName -like '$user*'" | Select-Object SamAccountName).SamAccountName){
+      Write-Output "  $uname"
+    }
+  }
+  else{
+    Write-Output "No match for '$user'"
+  }
 }
